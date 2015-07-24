2569876ae3059f85ae733442be6beef7acd807f3
Alejoss <alejoveintimilla@gmail.com>
2015-07-24 18:49:15 -0500

    wsgi heroku

--- letrasclub/wsgi.py
+++ letrasclub/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LetrasClub.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "letrasclub.settings")
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())

f8934f78460eb11b08b54aeec3f036b8c90c6687
Alejoss <alejoveintimilla@gmail.com>
2015-11-20 16:25:17 -0500

    remover sendgrid

--- letrasclub/settings.py
+++ letrasclub/settings.py
@@ -148,8 +148,10 @@ if HEROKU:
     SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ["SOCIAL_AUTH_GOOGLE_KEY"]
 
     # Sendgrid
+    """
     EMAIL_HOST = 'smtp.sendgrid.net'
     EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
     EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
+    """

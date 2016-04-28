682315c761449dd87d36cc2b221f4ac3ea8b734e
AlejandroVentimilla <alejoveintimilla@gmail.com>
2016-04-28 15:10:49 -0500

    reverse

--- perfiles/admin.py
+++ perfiles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from social.apps.django_app.default.models import UserSocialAuth
+#from social.apps.django_app.default.models import UserSocialAuth
 
 from perfiles.models import Perfil
 
 admin.site.register(Perfil)
-admin.site.register(UserSocialAuth)
+#admin.site.register(UserSocialAuth)

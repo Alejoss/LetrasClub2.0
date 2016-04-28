79b754c6fc1876c7f6e40dadeaf06f77f862eeb7
AlejandroVentimilla <alejoveintimilla@gmail.com>
2016-04-28 15:15:12 -0500

    bug alreadyregistered model

--- perfiles/admin.py
+++ perfiles/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-#from social.apps.django_app.default.models import UserSocialAuth
 
 from perfiles.models import Perfil
 
 admin.site.register(Perfil)
-#admin.site.register(UserSocialAuth)

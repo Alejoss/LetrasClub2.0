9fe035c2a4d54bae9b00d01c81df8a8d08020702
AlejandroVentimilla <alejoveintimilla@gmail.com>
2016-05-10 19:20:54 -0500

    nuevos mensajes de error

--- letrasclub/urls.py
+++ letrasclub/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 
 from libros import views
 
+from django.views.generic import TemplateView
+from django.views.defaults import page_not_found, server_error
+
+
 urlpatterns = [
     url(r'^$', views.main, name='main_redirect'),
     url(r'^libros/', include('libros.urls', namespace="libros")),
@@ -10,5 +14,11 @@ urlpatterns = [
     url(r'^perfil/', include('perfiles.urls', namespace="perfiles")),
     url(r'^comments/', include('comentarios.urls', namespace="comments")),
     url('', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Probar errores
+    url(r'^400/$', TemplateView.as_view(template_name='400.html')),
+    url(r'^403/$', TemplateView.as_view(template_name='403.html')),
+    url(r'^404/$', page_not_found),
+    url(r'^500/$', server_error),
 ]

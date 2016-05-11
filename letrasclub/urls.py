327c00bbd118a4036a1fe12bdd9329192a9a2282
AlejandroVentimilla <alejoveintimilla@gmail.com>
2016-05-10 19:23:30 -0500

    remover probar errores

--- letrasclub/urls.py
+++ letrasclub/urls.py
@@ -15,10 +15,4 @@ urlpatterns = [
     url(r'^comments/', include('comentarios.urls', namespace="comments")),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
-
-    # Probar errores
-    url(r'^400/$', TemplateView.as_view(template_name='400.html')),
-    url(r'^403/$', TemplateView.as_view(template_name='403.html')),
-    url(r'^404/$', page_not_found),
-    url(r'^500/$', server_error),
 ]
